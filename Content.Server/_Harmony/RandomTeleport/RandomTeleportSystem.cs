9a9eb94ea28bc66a9926178e972a690b3571cddf
Ermucat <NCramey09@gmail.com>
2025-07-24 21:41:47 -0400

    bl

---
+++ Content.Server/_Harmony/RandomTeleport/RandomTeleportSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+using Content.Shared._Harmony.RandomTeleport;
+using Content.Shared.Administration.Logs;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Map;
+using Robust.Shared.Random;
+
+namespace Content.Server._Harmony.RandomTeleport;
+
+public sealed class RandomTeleportSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<RandomTeleportEvent>(OnRandomTeleport);
+    }
+
+    private void OnRandomTeleport(RandomTeleportEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        var xform = Transform(args.Performer);
+        var mapPos = _xform.GetWorldPosition(xform);
+        var radius = args.Radius;
+        var gridBounds = new Box2(mapPos - new Vector2(radius, radius), mapPos + new Vector2(radius, radius));
+
+        var randomX = _random.NextFloat(gridBounds.Left, gridBounds.Right);
+        var randomY = _random.NextFloat(gridBounds.Bottom, gridBounds.Top);
+
+        var pos = new Vector2(randomX, randomY);
+
+        _xform.SetWorldPosition(args.Performer, pos);
+
+
+        MapCoordinates coords = _xform.GetMapCoordinates(args.Performer);
+
+        Spawn(args.Effect, coords);
+
+        _audio.PlayPvs(args.SoundSpecifier, args.Performer);
+
+        args.Handled = true;
+    }
+}
---
+++ Content.Shared/_Harmony/RandomTeleport/RandomTeleportEvents.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Actions;
+using Robust.Shared.Audio;
+
+namespace Content.Shared._Harmony.RandomTeleport;
+
+public sealed partial class RandomTeleportEvent : InstantActionEvent
+{
+    [DataField]
+    public float Radius = 7;
+
+    [DataField]
+    public string Effect = "Smoke";
+
+    [DataField]
+    public SoundSpecifier SoundSpecifier =  new SoundPathSpecifier("/Audio/Magic/blink.ogg");
+}
