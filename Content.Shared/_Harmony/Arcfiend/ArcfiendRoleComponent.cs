946467869808ed76d04a94b8cee7f572cfb756f4
Ermucat <NCramey09@gmail.com>
2025-09-08 19:45:53 -0400

    a

---
+++ Content.Shared/_Harmony/Arcfiend/ArcfiendRoleComponent.cs
@@ -0,0 +1,11 @@
+﻿using Content.Shared.Roles.Components;
+
+namespace Content.Shared._Harmony.Arcfiend;
+
+/// <summary>
+/// Added to mind role entities to tag that they are a arcfiend.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ArcfiendRoleComponent : BaseMindRoleComponent
+{
+}
